10499d15338b4704091318a5a91e7b4823144f8f
Mateus Silva <theusskt2018@gmail.com>
2021-06-16 15:48:54 -0300

    Lesson, Content, Task

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -29,7 +29,7 @@ public class User implements Serializable{
 	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "tb_user_role",
 	    joinColumns = @JoinColumn(name = "user_id"),
-	    inverseJoinColumns = @ JoinColumn(name = "role_id"))
+	    inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
 	
 	public User() {
